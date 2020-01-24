9dbc039b01ac544c91ed566a1864502b1ae2f39a
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2020-01-25 01:09:35 +0200

    Initial tests for distance calculation

--- src/utils.py
+++ src/utils.py
@@ -61,7 +61,8 @@ def euclidean_distance(user_location, restaurant_location):
     https://en.wikipedia.org/wiki/Meridian_arc
     https://en.wikipedia.org/wiki/Latitude#Length_of_a_degree_of_latitude
     '''
-
+    # Circumference of a circle = 2 * pi * r, earth`s radius is ~6371km
+    # -> (2 * pi * 6371) / 360 = ~111km
     length_of_a_degree = 111e3  # meters
     distance_lon = restaurant_location[0] - user_location[0]
     distance_lat = restaurant_location[1] - user_location[1]
