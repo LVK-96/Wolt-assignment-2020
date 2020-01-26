1f74f06d2658781bcaad1e0c3c02aa1dae5580d1
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2020-01-26 13:13:53 +0200

    Improve api tests

--- test/test_api.py
+++ test/test_api.py
@@ -3,6 +3,14 @@ import unittest
 
 
 class TestAPI(unittest.TestCase):
+    def check_restaurant_has_all_fields(self, restaurant):
+        keys = ['city', 'currency', 'delivery_price', 'description', 'image',
+                'location', 'name', 'online', 'tags', 'blurhash']
+        for k in keys:
+            if k not in restaurant:
+                return False
+        return True
+
     def setUp(self):
         self.client = app.test_client()
         self.client.testing = True
@@ -57,10 +65,22 @@ class TestAPI(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(body), 0)
 
-    def test_valid_query_that_has_mathcing_restaurants(self):
+    def test_valid_quefy_that_has_a_mathcing_restaurant(self):
         response = self.client.get(
             '/restaurants/search?q=momotoko&lat=60.17045&lon=24.93147'
         )
         body = response.get_json()
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(body), 1)
+        self.assertTrue(self.check_restaurant_has_all_fields(body[0]))
+        self.assertTrue(body[0]['name'] == 'Momotoko Citycenter')
+
+    def test_valid_query_that_has_mathcing_restaurants(self):
+        response = self.client.get(
+            '/restaurants/search?q=sushi&lat=60.17045&lon=24.93147'
+        )
+        body = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(body), 9)
+        for r in body:
+            self.assertTrue(self.check_restaurant_has_all_fields(r))
